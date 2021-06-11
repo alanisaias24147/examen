e4b2491c1e6c2907037cbb9dea83a0ab026ae163
alanisaias24147 <78932267+alanisaias24147@users.noreply.github.com>
2021-06-11 07:49:24 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace pendientes
+{
+    class Pendientes
+    {
+
+	
+        List<string> pendientes = new List<string>();
+
+        public void push(string pendiente)
+        {
+            this.pendientes.Add(pendiente);
+        }
+
+        public string pop()
+        {
+            string value = this.pendientes[this.pendientes.Count - 1];
+            this.pendientes.RemoveAt(this.pendientes.Count - 1);
+            return value;
+        }
+
+        public string peek()
+        {
+            return this.pendientes[this.pendientes.Count - 1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using pendientes;
+
+namespace examen_3er_parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pendientes pendientes = new pendientes;
+            pendientes.push();
+            pendientes.push();
+            pendientes.push();
+
+            string opcion = "";
+            while (opcion != "9")
+            {
+                opcion = "";
+
+                while (opcion != "1" && opcion != "2" && opcion != "3")
+                {
+                    Console.WriteLine("ingresa tu nombre");
+                    string pendiente = Console.ReadLine();
+                    Console.WriteLine("2)agregar pendiente");
+                    Console.WriteLine("4)quitar pendiente");
+                    Console.WriteLine("6)checar pendiente");
+                    Console.WriteLine("8) salir");
+
+                    opcion = Console.ReadLine();
+
+                    if (opcion != "1" && opcion != "2" && opcion != "9")
+                    {
+                        Console.WriteLine("La opcion seleccionada no es correcta");
+                    }
+                }
+
+                if (opcion == "2")
+                {
+                    Console.WriteLine("agregar nota");
+                    string nombre = Console.ReadLine();
+                }
+                else if (opcion == "4")
+                {
+                    Console.WriteLine("quitar pendiente");
+                    string nombre = Console.ReadLine();
+                }
+                else if (opcion == "6")
+                {
+                    Console.WriteLine("checar pendiente");
+                }
+                else if (opcion == "8")
+                {
+                    Console.WriteLine("salir");
+                    
+                    
+                }
+            }
+        }
+    }
+}
